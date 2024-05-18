c6b99680fa1f534668c25659bf339ef0b8aae3ee
Michael Pedersen <michaelp@discoverdst.com>
2024-05-17 22:56:59 -0400

    lol actually don't need a controller. spring does it itself

--- backend/src/main/java/com/goia/feedafish/TestController.java
+++ backend/src/main/java/com/goia/feedafish/TestController.java
@@ -1,26 +1,10 @@
 package com.goia.feedafish;
 
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-import java.util.List;
-
 @RestController
 public class TestController {
-    private final ResourceHttpRequestHandler resourceHandler;
-
-    public TestController() {
-        this.resourceHandler = new ResourceHttpRequestHandler();
-        Resource location = new ClassPathResource("static/");
-        this.resourceHandler.setLocations(List.of(location));
-    }
-
+    
     @GetMapping("/getLatestFish")
     public Fish getLatestFish() {
         return Fish.generateRandomFish();
