2b83650cc20896e76d010dc9660522df9d844629
Dimanor3 <bijanrazavi15@gmail.com>
2024-05-17 21:29:44 -0400

    Initial Backend Commit.

---
+++ backend/src/main/java/com/goia/feedafish/FeedafishApplication.java
@@ -0,0 +1,13 @@
+package com.goia.feedafish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedafishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedafishApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/goia/feedafish/FeedafishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goia.feedafish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedafishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
