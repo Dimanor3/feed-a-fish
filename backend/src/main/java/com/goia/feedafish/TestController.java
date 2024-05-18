a545c6df09b045f3bb741d1ca3b419f9e0bfd3d6
Michael Pedersen <michaelp@discoverdst.com>
2024-05-17 23:14:05 -0400

    add /generate/fish

--- backend/src/main/java/com/goia/feedafish/TestController.java
+++ backend/src/main/java/com/goia/feedafish/TestController.java
@@ -1,12 +1,27 @@
 package com.goia.feedafish;
 
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Autowired
+    private DataSource dataSource;
+    
     @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/generate/fish")
+    public Fish generateFishEndpoint() {
+        Fish newFish = Fish.generateRandomFish();
+        newFish.saveToDatabase(dataSource);
+        return newFish;
     }
 }
