81e9a301f5a43aa01731b21b0805bb6c029ae7e9
Dimanor3 <bijanrazavi15@gmail.com>
2024-05-20 01:54:17 -0400

    Backend: Schedule wasn't working, added EnableScheduling to fix it.

--- backend/src/main/java/com/goia/feedafish/FeedafishApplication.java
+++ backend/src/main/java/com/goia/feedafish/FeedafishApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.Statement;
 
 @SpringBootApplication
+@EnableScheduling
 public class FeedafishApplication {
 
 	public static void main(String[] args) {
